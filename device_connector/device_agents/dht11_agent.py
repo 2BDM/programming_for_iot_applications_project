01716c21f4e3043699220a15f19a05333c81a628
Giovanni Dettori <giovi.detto2000@gmail.com>
2023-01-06 10:43:45 +0100

    first tentative of dht11 sensor

--- device_connector/device_agents/dht11_agent.py
+++ device_connector/device_agents/dht11_agent.py
@@ -1,5 +1,18 @@
-#import
+import random as rnd
 
 class DHT11Agent:
     def __init__(self):
-        pass
+        self.type = "DHT11"
+        self.dict_temp_hum = {"humidity":0,"temperature":0}
+        
+    def take_measure(self):
+        self.updateTemp()
+        self.updateHum()
+        return self.dict_temp_hum
+    
+    def updateTemp(self):
+        self.dict_temp_hum["temperature"]=rnd.randint(18,25)
+        
+    
+    def updateHum(self):
+        self.dict_temp_hum["humidity"]=rnd.randint(50,80)    
