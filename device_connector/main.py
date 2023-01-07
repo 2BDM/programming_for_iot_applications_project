a7b622f9783cd8a09344e1ba98d8081562cc8bbd
Davide Macario <dav.macario@gmail.com>
2023-01-07 01:08:41 +0100

    Update device connector main program to connect to MQTT broker, make measurements and publish them

--- device_connector/main.py
+++ device_connector/main.py
@@ -8,6 +8,21 @@ from sub.MyMQTT import MyMQTT
 
 # TODO: add libraries for sensors - need RPi
 
+
+def searchListOfDict(lst, parameter, value):
+    """
+    Used to search for elements inside a list of dictionaries
+    `lst`.
+    The search is performed over key `parameter`, having value 
+    `value`.
+    If found, return it, else return None
+    """
+    for elem in lst:
+        if elem[parameter] == value:
+            return elem
+    return None
+
+
 ### Define main class
 class DevConn:
     def __init__(self, conf_path, self_path):
@@ -16,11 +31,11 @@ class DevConn:
         - Configuration file (with services catalog location)
         - Information about self
         """
-        with open('conf_path') as f:
+        with open(conf_path) as f:
             self.conf = json.load(f)
             self.sc_addr = self.conf["services_catalog"]["ip"] + str(self.conf["services_catalog"]["port"])
 
-        with open('self_path') as f:
+        with open(self_path) as f:
             self.whoami = json.load(f)
 
         self.dev_cat_info = {}
@@ -42,6 +57,12 @@ class DevConn:
         for elem in self.whoami["resources"]["sensors"]:            
             # Add the 'last measured' field
             # Iterate over the measure_type list
+
+            # Sensors can measure different quantities - each sensor is 
+            # associated to a sub-list in `self.last_meas`.
+            
+            # `self.last_meas` follows the same indexing as `self.dev_agents_sens`
+            sens_meas_sublist = []
             for ind in range(len(elem["measure_type"])):
                 # NOTE: SenML format
                 # Assume no sensors measure the same quantity 
@@ -53,7 +74,11 @@ class DevConn:
                     "t": 0,
                     "v": 0
                     }
-                self.last_meas.append(curr_meas)
+
+                if searchListOfDict(sens_meas_sublist, "n", curr_meas["n"]) is None:
+                    sens_meas_sublist.append(curr_meas)
+            
+            self.last_meas.append(sens_meas_sublist)
 
             # TODO: initialize correct objects - need RPi
             # NOTE: Pin numbers are found in the configuration file
@@ -61,6 +86,7 @@ class DevConn:
             if elem["device_name"] == "DHT11":
                 self.dev_agent_ind_sens["DHT11"] = count
                 count += 1
+                # Here one can add the specific instantiations of the device agents
                 #
                 # self.dev_agents_sens.append(DHT11())
                 pass
@@ -99,16 +125,40 @@ class DevConn:
             notifier=self
             )
 
+        # Connect to the broker
+        self.mqtt_cli.start()
+
+        # Subscribe to the topics used for control the actuators
+        # The topics are stored in the list `act_topics`
+        act_topics = []
+        for elem in self.whoami["resources"]["actuators"]:
+            if "MQTT" in elem["available_services"]:
+                for top in elem["service_details"]["topic"]:
+                    act_topics.append(top)
+                    self.mqtt_cli.mySubscribe(top)
+        
+
+
+
     #TODO
     ##################################################################
-    def notify(self):
+    def notify(self, topic, payload):
         """
         Callback for the MyMQTT object
         When a message is received in either of the 
         topics related to the actuators, it acts on them.
         """
         # Read message
-        # Depending on the topic and content, choose the most suitable action
+        # Depending on the topic and content, choose the right action
+
+        # Find right actuator depending on topic
+        for act in self.whoami["resources"]["actuators"]:
+            if "MQTT" in act["available_services"]:
+                if topic in act["service_setails"]:
+                    # Found correct device!
+                    # ...
+                    pass
+
         pass
     ##################################################################
 
@@ -129,6 +179,7 @@ class DevConn:
             return 1
         else:
             return 0
+
 
     def connectToServCat(self, max_tries):
         """
@@ -241,9 +292,37 @@ class DevConn:
             return -1
 
 
+    def updateMeas(self):
+        # Read all available services and update self.last_meas
+
+        for sensname in self.dev_agent_ind_sens:
+            # Each device agent must have a method 'measure()'
+            # `curr_meas` is already a SenML-formatted Python dictionary
+            curr_meas = self.dev_agents_sens[self.dev_agent_ind_sens[sensname]].measure()
+            
+            # Find the correct sublist containing the last measurement for the sensor 
+            # and place the current measurement inside
+
+            # curr_meas can be either a single dict (same device used to 
+            # measure multiple quantities) or a list of dicts
+            if isinstance(curr_meas, dict):
+                self.last_meas[self.dev_agent_ind_sens[sensname]] = [curr_meas]
+            
+            elif isinstance(curr_meas, list):
+                self.last_meas[self.dev_agent_ind_sens[sensname]] = curr_meas
+
+    
+    def publishLastMeas(self):
+        # Read all sensor topics from "self.whoami"
+        # Find the last measured value for the specific quantity (use indexing in `self.dev_agent_ind_sens`)
+        # Publish it in the correct topic
+        pass
+
 
 
 ### Main program - include loop
 
 if __name__ == "__main__":
-    pass
+    
+    myDevConn = DevConn("./conf_dev_conn.json", "./dev_info.json")
+    
