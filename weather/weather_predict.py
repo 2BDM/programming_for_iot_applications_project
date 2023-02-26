2ffb4196628df2851ac91c7516f8ca7577d33791
Davide Macario <dav.macario@gmail.com>
2023-02-26 19:21:50 +0100

    Add initial draft for weather station and prediction strategy programs

---
+++ weather/weather_predict.py
@@ -0,0 +1,8 @@
+import numpy as np
+import pandas as pd
+import sklearn
+
+"""
+This program is used to make weather prediction using the sensor data and having
+as training set the historical data about the weather
+"""
---
+++ weather/weather_station.py
@@ -0,0 +1,11 @@
+import cherrypy
+import json
+import time
+import sys
+from sub.MyMQTT import MyMQTT
+
+"""
+This is the main program for the weather station.
+
+
+"""
