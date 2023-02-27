5017dfa2e11989657982f70f6976003b67f2537e
Davide Macario <dav.macario@gmail.com>
2023-02-27 23:32:13 +0100

    Update weather prediction - bad performance for now (basically random guess)

--- weather/weather_predict.py
+++ weather/weather_predict.py
@@ -1,15 +1,35 @@
 import numpy as np
 import pandas as pd
-from sklearn.linear_model import Ridge
-from sklearn.ensemble import RandomForestClassifier
+import random
+
+from sklearn.neural_network import MLPClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC
+from sklearn.gaussian_process import GaussianProcessClassifier
+from sklearn.gaussian_process.kernels import RBF
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis
 
 """
 This program is used to make weather prediction using the sensor data and having
 as training set the historical data about the weather
 """
 
+# TODO: use a classifier on the "target" values.
+# TODO: try to find a way to only use the seasonal information as training set
+# TODO: to perform validation, shuffle the data
+
+## It may be that, by not shuffling, the model can pick up patterns 
+# better among the data - will need to check
+
+
 tr_set_file = "weather_data/weather_train.csv"
-weather_df = pd.read_csv(tr_set_file)
+try:
+    weather_df = pd.read_csv(tr_set_file)
+except:
+    weather_df = pd.read_csv(("weather/" + tr_set_file))
 
 # Count missing values:
 print("Fraction of missing values: ", weather_df.isnull().sum()/len(weather_df.index))
@@ -23,8 +43,16 @@ weather_df["target"] = weather_df.shift(-1)["FENOMENI"].ffill()
 print(weather_df)
 print(weather_df.corr())
 
+# Stats
+n_tot = len(weather_df.index)
+n_rain = weather_df["FENOMENI"].sum()
+n_no_rain = n_tot - n_rain
+
+print(f"Number of rain records: {n_rain}\nNumber of no rain: {n_no_rain}")
+print(f"Fraction of no rain: {n_no_rain/n_tot}")
+
 #rr = Ridge(alpha=.1)
-rr = RandomForestClassifier()
+rfc = KNeighborsClassifier(n_neighbors=20)
 
 # Regressors - labels of the columns used as regressors
 regressors = weather_df.columns[~weather_df.columns.isin(["target", "STAGIONE"])]
@@ -57,25 +85,55 @@ def backtest(weather, model, regressors, start=550, step=90):
 
     return pd.concat(all_predictions)
 
-predictions_backtested = backtest(weather_df, rr, regressors)
-
+predictions_backtested = backtest(weather_df, rfc, regressors)
 print(predictions_backtested)
-
-# TODO: use a classifier on the "target" values.
-# TODO: try to find a way to only use the seasonal information as training set
-# TODO: to perform validation, shuffle the data
-
-## It may be that, by not shuffling, the model can pick up patterns 
-# better among the data - will need to check
 
 # Accuracy
 miss = predictions_backtested['diff'].sum()/len(predictions_backtested.index)
 acc = 1 - miss
 
-print("Accuracy (backtesting): ", acc)
+print("Accuracy (backtesting) with random forest: ", acc)
 
 #######################################################################################
 # Random split:
 
 # Shuffle data:
+sh_range = list(range(len(weather_df.index)))
+random.shuffle(sh_range)
 
+sh_weather = weather_df.iloc[sh_range]        # SHUFFLED DF
+
+n_elem = len(sh_weather.index)
+n_train = round(0.7*n_elem)
+n_test = n_elem - n_train
+
+train_set = sh_weather.iloc[:n_train]
+test_set = sh_weather[n_train:]
+
+# Check
+print(f"\nTot len: {n_elem}\nTr len: {len(train_set.index)}\nTest len: {len(test_set.index)}\n")
+
+rfc.fit(train_set[regressors], train_set["target"])
+predict = rfc.predict(test_set[regressors])
+
+# Make result a DF
+predict = pd.Series(predict, index=test_set.index)
+
+# Include info about actual classes
+out = pd.concat([test_set["target"], predict], axis=1)
+out.columns = ["actual", "prediction"]
+
+acc_rfc = (out["actual"] == out["prediction"]).sum()/n_test
+
+print(f"Accuracy - random forest on shuffled data: {acc_rfc}")
+
+### COMMENTS
+# The accuracy is basically the same for the 2-class classifier, meaning backtesting is not
+# fundamental (probably)
+
+### TODO: create one classifier per season
+
+
+
+################################################
+# Prediction of instant weather
