6aaeb54343e748f7073cafbe89b7873acb935be8
Davide Macario <dav.macario@gmail.com>
2023-03-02 00:02:28 +0100

    Updated weather station program - finished main class

--- weather/weather_station.py
+++ weather/weather_station.py
@@ -1,11 +1,16 @@
 import cherrypy
 import json
 import time
+import pandas as pd
 from datetime import datetime
 import sys
 from sub.MyMQTT import MyMQTT
 import requests
 import warnings
+import pickle
+
+from weather_predict import COLS_CURRENT, COLS_FUTURE
+from weather_predict import currWeather_2, futureRain_2
 
 """
 This is the main program for the weather station.
@@ -40,6 +45,10 @@ MONTH2SEASON = {
     '11': 'fall',
     '12': 'winter'
 }
+
+# Lower bounds for light intensity
+LIGHT_SUNNY = 10000         # Lux
+LIGHT_CLOUDY = 1000         # Lux
 
 def datetime2monthNumber(dt):
     return int(dt.strftime("%m"))
@@ -81,13 +90,11 @@ class WeatherStation():
             "Light Intensity"
         }
 
-        # TODO: decide and initialize the data structure to store past measurements into
         # Idea: it must contain measurements relative to the lst 24 hours, which are then updloaded to 
         # MongoDB every [HOW MANY?] hours
 
         # SenML syntax:
         #     {'n': name_of_meas, 'u': units, 't': timestamp, 'v': value}
-        
         # MQTT message syntax:
         #     {'bn': 'base_name', 'e': {'n': name_of_meas, 'u': units, 't': timestamp, 'v': value}}
 
@@ -107,6 +114,7 @@ class WeatherStation():
         """
         self.devices_meas = {}
 
+
     def updateTime(self):
         """
         Automatically update current time, date and season
@@ -116,6 +124,7 @@ class WeatherStation():
         self.current_time = self.current_datetime.strftime("%H:%M")
         self.current_date = self.current_datetime.strftime("%Y-%m-%d")
         self.current_season = MONTH2SEASON[str(datetime2monthNumber(self.current_date))]
+
 
     def addMeasurement(self, newSenML, dev_id=None):
         """
@@ -158,6 +167,7 @@ class WeatherStation():
             # Hopefully never...
             return 0
 
+
     def cleanupMeas(self):
         """
         Used to remove old measurements.
@@ -196,21 +206,198 @@ class WeatherStation():
 
         return n_del
 
-    def evalMeasPrediction(self, ):
+
+    def evalMeasPrediction(self, time_range, dev_id):
         """
         Used to evaluate the measurements for the prediction strategy.
         It also adds the season using the current date.
-
+        ---
+        The parameter 'time_range' is used to only consider a specific time period to 
+        get the measurements from (e.g., 4 hours). It is in seconds.
+        ---
+        This method is not (typically) called directly, rather it is called by 
+        estCurrWeather or estFutureWeather.
+        ---
         Measurements for weather:
         - Average temp (cel)
         - Min temp (cel)
         - Max temp (cel)
         - Humidity (%)
-        - Pressure (mb relative to sea level) --> Use convertion method
+        - Pressure (mb relative to sea level) --> Use conversion method (bar2mb_sealevel)
         """
         self.updateTime()
 
-        pass
+        id = str(dev_id)
+
+        my_meas = self.relevant_meas.copy()
+        my_meas.remove("Light Intensity")
+
+        # taken_cat contains the measurements which are to be used
+        # Keys are the measurements names
+        taken_cat = {}
+        for n in my_meas:
+            taken_cat[n] = []
+
+        # Get the measurements from the dict
+        for n in my_meas:
+            for meas in self.devices_meas[id][n]:
+                if (self.current_timestamp - meas['t']) <= time_range:
+                    # Keep measurement
+                    if n == "Air Pressure" and meas['u'] == "Bar":
+                        # Need to obtain the barometric pressure in mb 
+                        # relative to sea level
+                        taken_meas = bar2mb_sealevel(meas['v'])
+                    else:
+                        taken_meas = float(meas['v'])
+
+                    taken_cat[n].append(taken_meas)
+        
+        # Evaluate actual values:
+
+        # Mean, min, max temperature:
+        t_mean = sum(taken_cat["Temperature"])/len(taken_cat["Temperature"])
+        t_min = min(taken_cat["Temperature"])
+        t_max = max(taken_cat["Temperature"])
+
+        # Mean humidity:
+        h_mean = sum(taken_cat["Humidity"])/len(taken_cat["Humidity"])
+
+        # Mean pressure:
+        p_mean = sum(taken_cat["Air Pressure"])/len(taken_cat["Air Pressure"])
+
+        # Season:
+        seas = MONTH2SEASON[str(datetime2monthNumber(self.current_datetime))]
+        seas_id = SEASON2ID[seas]
+
+        out = pd.Series([t_mean, t_min, t_max, h_mean, p_mean, seas_id], index=COLS_CURRENT)
+        
+        return out
+
+
+    def estCurrWeather(self, device_id, time_range=7400, test_el=None, model=None, model_path="models/weather_today.sav"):
+        """
+        Estimate the current weather from data obtained by the sensors.
+        The considered data is given by the maximum age of specified in
+        time_range.
+        The method 'validates' the result with the use of the light sensor, 
+        but only if the prediction is done at daytime.
+        ---
+        Input parameters:
+        - device_id (int): id of the device whose measurements have to be used
+        - time_range (int, default 7200): maximum age of the considered data - in s
+        - test_el (pd.Series, default None): optional input data alreafy evaluated.
+        - model (sklearn model, fitted, default None): optional model to be used,
+        must be already trained
+        - model_path (default models/weather_today.sav): path specifying the model 
+        (.sav file that can be opened with pickle)
+
+        Note: the input data must be formatted having the following columns:
+            'TMEDIA °C', 
+            'TMIN °C', 
+            'TMAX °C', 
+            'UMIDITA %', 
+            'PRESSIONESLM mb', 
+            'STAGIONE'
+        ---
+        Output values:
+        - 1, 'day': rain, tested with light intensity
+        - 0, 'day': sunny, tested with light intensity
+        - -1, 'day': cloudy, tested with light intensity
+        - 1, 'night'
+        - 0, 'night'
+        """
+        # The sklearn model is used to estimate the presence of rain. This 
+        # estimate is then compared with the measurements about the light 
+        # intensity of today, hence it is also possible to estimate the 
+        # weather as simply 'cloudy'
+        # Alternatives: sunny, cloudy, rainy
+
+        self.updateTime()
+
+        if test_el is None:
+            # If here, the device ID MUST BE PRESENT
+            test_el = self.evalMeasPrediction(time_range, device_id)
+
+        if model is None:
+            # Extract model:
+            with open(model_path, 'rb') as f:
+                load_model = pickle.load(f)
+        else:
+            load_model = model
+        
+        # Perform prediction: 1 if rain/precipitation, else 0
+        pred_weather = currWeather_2(test_el, load_model)           # Series object
+
+        # Illumination ranges are defined as global variables
+        
+        # The light analysis needs to be done only when it is NOT NIGHT
+        curr_hour = int(self.current_time.split(':')[0])
+
+        light_lst = []
+        for meas in self.devices_meas[device_id]["Light Intensity"]:
+            # The light measurements which are the ones less old than half an hour
+            # ALWAYS
+            if (self.current_timestamp - meas['t']) < 1800:
+                light_lst.append(meas['v'])
+        
+        light_avg = sum(light_lst)/len(light_lst)
+
+        # Moonlight reaches values of 0.1... 
+        # --> this should be enough to detect day vs. night
+        if light_avg > 2 and curr_hour > 5 and curr_hour < 23:
+            # Validation of the results
+            rain = pred_weather.values
+            if not rain:
+                # look at light: if low then cloudy, else
+                if light_avg > LIGHT_SUNNY:
+                    return 0, 'day'
+                else:
+                    return -1, 'day'
+            else:
+                return rain, 'day'
+        else:
+            return pred_weather.values, 'night'
+
+
+    def estFutureWeather(self, device_id, time_range=24*3600, test_el=None, model=None, model_path="models/weather_today.sav"):
+        """
+        Perform estimation of precipitations for the next day, given the values gathered in the
+        last 'time_range' (default 24 hours) and today's precipitations.
+        ---
+        Input parameters:
+        - device_id (int): id of the device whose measurements have to be used
+        - time_range (int, default 24*3600): maximum age of the considered data - in s
+        - test_el (pd.Series, default None): optional input data alreafy evaluated.
+        - model (sklearn model, fitted, default None): optional model to be used,
+        must be already trained
+        - model_path (default models/weather_today.sav): path specifying the model 
+        (.sav file that can be opened with pickle)
+        """
+        # To get the future weather (done typically once a day at 10 pm), the 
+        # model needs to know whether it rained today. Therefore, before the 
+        # actual prediction is done, it is necessary to evaluate the presence 
+        # of rain in the past 24 hours
+
+        self.updateTime()
+
+        if test_el is None:
+            test_el = self.evalMeasPrediction(time_range, device_id)
+
+        if model is None:
+            # Extract model:
+            with open(model_path, 'rb') as f:
+                load_model = pickle.load(f)
+        else:
+            load_model = model
+        
+        pred_today_rain = self.estCurrWeather(device_id, time_range, test_el)[0]
+
+        new_test = test_el.copy()
+        new_test["FENOMENI"] = pred_today_rain
+
+        pred_tomorrow_rain = futureRain_2(new_test, load_model)
+
+        return pred_tomorrow_rain.values
 
 
 
